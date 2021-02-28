22c519999c80a634edce31d937b585248dc9f0fc
梓桐 王 <573922610@qq.com>
2021-02-28 18:08:03 +0800

    test成功，现将作业文件复制进入此库上传！

---
+++ HW_1/1.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = " ";
+            double a = 0;
+            double b = 0;
+            string c = " ";
+            Console.Write("请输入第一个运算数字：");
+            s = Console.ReadLine();
+            a = Int32.Parse(s);
+            Console.Write("请输入第二个运算数字：");
+            s = Console.ReadLine();
+            b = Int32.Parse(s);
+            Console.Write("请输入一个运算符号：");
+            c = Console.ReadLine();
+
+            switch (c)
+            {
+                case "+":
+                    Console.WriteLine($"{a} {c} {b} = {a + b}");
+                    break;
+                case "-":
+                    Console.WriteLine($"{a} {c} {b} = {a - b}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{a} {c} {b} = {a * b}");
+                    break;
+                case "/":
+                    Console.WriteLine($"{a} {c} {b} = {a / b}");
+                    break;
+
+            }
+        }
+    }
+}
