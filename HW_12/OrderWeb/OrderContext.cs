f95bab374644408b79f89fd1240fcacf339a8b23
梓桐 王 <573922610@qq.com>
2021-05-18 22:30:24 +0800

    已完成

---
+++ HW_12/OrderWeb/OrderContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using T8;
+using T6;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderWeb
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
